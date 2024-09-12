7669b77dc48ab8a04ac985fbe70fba95969943d2
bilal-raza12 <muhammadbilalraza497@gmail.com>
2024-09-13 01:54:52 +0500

    Random Joke Generator App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import RandomJokeComponent from "@/components/random-joke";
+
+export default function Home() {
+  return (
+    <div >
+      <RandomJokeComponent />
+    </div>
+  );
+}
